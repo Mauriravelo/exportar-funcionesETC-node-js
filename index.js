0b5429eed509bacbd317ecb244b8ddb7b03a97b7
Mauriravelo <mauricioravelo48@gmail.com>
2024-02-06 19:40:44 -0300

    primer commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const match = require('./match')
+
+console.log(match)
+/*
+console.log(match.add(1,0))
+console.log(match.substract(1,0))
+console.log(match.multiply(1,0))
+console.log(match.divide(1,0))
+console.log(match.divide(2,1))
+*/
